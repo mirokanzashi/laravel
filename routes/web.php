b83d624ab385f8124e4ff52a0790ff06e1dccdad
mirokanzashi <nirekagami@gmail.com>
2018-03-20 16:39:40 +0900

    first commit

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+/*
+Route::get('/hello',function(){
+    return 'Hello World!';
+});*/
+Route::get('hello', 'Hello@index');
+Route::get('/hello/{name}', 'Hello@show');
